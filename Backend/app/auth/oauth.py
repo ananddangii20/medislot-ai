3bef8aa55de86a91119f4d86ec0aa2032eea3615
Anand Dangi <ananddangii20@gmail.com>
2026-04-08 11:05:09 +0530

    fixed

--- Backend/app/auth/oauth.py
+++ Backend/app/auth/oauth.py
@@ -1,11 +1,13 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "./firebase";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function googleLoginBackend() {
   const result = await signInWithPopup(auth, provider);
   const user = result.user;
 
-  const res = await fetch("http://127.0.0.1:8000/auth/google", {
+  const res = await fetch(`${API_URL}/auth/google`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
